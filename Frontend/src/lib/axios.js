f101cc962a4d07361b3eaeb552b24e370df2bd51
Uday Gadhavi <udaygadhavi2410@gmail.com>
2026-05-20 15:07:50 +0530

    Remove .env file and update Axios baseURL to use environment variable

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a reusable Axios instance targeting your backend port
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: proccess.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
     headers: {
         'Content-Type': 'application/json',
     },
